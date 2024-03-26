c69c4b3608ee9d31ca882e2006cfb48898acef9c
Stefan Giba <stefang867@gmail.com>
2024-03-26 19:40:15 +0000

    remove custom opts for gitsigns

--- init.lua
+++ init.lua
@@ -247,15 +247,7 @@ require('lazy').setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
+    opts = {},
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
